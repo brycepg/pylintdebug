5451630038ed3218862690a0808aee07bdff37ae
Bryce Guinta <bryce.paul.guinta@gmail.com>
2017-12-30 23:02:09 -0700

    Add first working commands

--- pylintdebug/cli.py
+++ pylintdebug/cli.py
@@ -1,14 +1,41 @@
+import sys
 import argparse
+
 import pylint
 from pylint.lint import Run
 
+from .func import print_callsite_location
+
+
+def monkeypatch_add_message():
+    """Monkeypatch the pylint base add_message method to print
+    callsite location
+
+    Every message that is added to pylint passes through
+    this method
+    """
+    print("Monkeypatching add_message")
+    method_path = pylint.checkers.BaseChecker.add_message
+    pylint.checkers.BaseChecker._debug_add_message = method_path
+    pylint.checkers.BaseChecker.add_message = debug_add_message
+
+def debug_add_message(*args, **kwargs):
+    print_callsite_location()
+    return pylint.checkers.BaseChecker._debug_add_message(*args, **kwargs)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('path')
-    parser.add_argument('--messages', action='store_true', default=False)
+    parser.add_argument('--messages-location', action='store_true', default=False, help="Get location of add_message in pylint source tree")
+    parser.add_argument('--pylint-messages', action='store_true', help='enable all pylint messages')
     args = parser.parse_args()
-    if args.messages:
-        print("MESSAGES")
-        print(pylint.checkers.BaseChecker.add_message)
-    Run([args.path])
+    if args.messages_location:
+        monkeypatch_add_message()
+    if not args.messages_location:
+        print("No debug options given for {}".format(sys.arv[0]))
+    pylint_args = []
+    if args.pylint_messages:
+        pylint_args.append('--enable=all')
+    else:
+        pylint_args.append('--disable=all')
+    Run([args.path, '-rn', '-sn'] + pylint_args)
---
+++ pylintdebug/func.py
@@ -0,0 +1,66 @@
+"""Debugging functions
+
+func_info:
+
+Decorator to print args and return values
+This helps understanding chain of events in code. The downside
+is you need to know
+
+
+print_callsite_location:
+
+Prints the parent file, line location of the current function
+This helps finding where method calls to widely used interfaces occur in code
+"""
+import sys
+import inspect
+from collections import abc
+
+
+try:
+    from astroid.node_classes import NodeNG
+    HAS_ASTROID = True
+except ImportError:
+    HAS_ASTROID = False
+
+def func_info(func=None, **options):
+    """Decorator to retrieve function info
+
+    Allows for additional keyword arguments in decorator.
+
+    Keyword Args:
+        listify: If true listify generators to make them printable
+    """
+    if func is None:
+        def partial_infer(func):
+            return func_info(func, **options)
+        return partial_infer
+    def inner(*args, **kwargs):
+        convert_generators = options.get("listify", False)
+        print("Calling", func.__qualname__)
+        for arg in args:
+            if HAS_ASTROID and isinstance(arg, NodeNG):
+                if "\n" not in arg.as_string() and len(arg.as_string()) < 80:
+                    print("Node: ", arg.as_string())
+                else:
+                    print("Node: ", arg)
+        if args:
+            print("Args:", args)
+        if kwargs:
+            print("kwargs:", kwargs)
+        result = func(*args, **kwargs)
+        if isinstance(result, abc.Generator) and convert_generators:
+            result = list(result)
+        print("result", result)
+        print("-------")
+        print()
+        if convert_generators:
+            result = iter(result)
+        return result
+    return inner
+
+
+def print_callsite_location():
+    """Print location of function call of the parent of the call location."""
+    fi = inspect.getouterframes( inspect.currentframe() )[2]
+    print("+%s %s %s" % (fi.lineno, fi.filename, fi.function))
